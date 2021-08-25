f895e356d1993e86473c3163c533a85b4a036487
ThusithaHerath <thusithalherath@gmail.com>
2021-08-25 17:19:06 +0530

    i did this project to learn pure php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+
+$hostname = "localhost";
+$username = "root";
+$password ="";
+$database ="usermanagement";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die("Database connection failed");
+
+?>
